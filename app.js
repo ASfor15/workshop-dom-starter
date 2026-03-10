1227f9a877857454f7211d486d6b9a1f265a431a
Akshara Singel <asinge01@student.bbk.ac.uk>
2026-03-10 20:50:01 +0000

    Interaction and events workshop completed.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ document.body.innerHTML = `
     <h1>Eurovision song contest - 2000s Results</h1>
     <p> Top 5  places for each year </p>
     <br>
+
+    <input type="text" id="search-input" placeholder="Search by song, artist or country"/>
+    <ul id="search-result"></ul>
+    <br>
     
     <h2 id="first-place-header"></h2>
     <ul id="first-place-list"></ul>
@@ -54,4 +58,20 @@ for (const person of contestants) {
         }
     }
 
+const searchInput = document.getElementById('search-input')
+const searchResults = document.getElementById('search-result')
 
+searchInput.addEventListener('input', function () {
+    const query = searchInput.value
+    searchResults.innerHTML = ''
+
+    if (query === '') return 
+
+    for (const person of contestants) {
+        if (person.song && person.song.includes(query) ||
+            person.performer && person.performer.includes(query) ||
+            person.toCountry && person.toCountry.includes(query)) {
+            searchResults.innerHTML += '<li>' + person.year + ' - ' + person.song + ' by ' + person.performer + ' (' + person.toCountry + ') </li> '
+        }
+    }
+})
